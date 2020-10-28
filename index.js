bea47cfdb2d2b4861bf8a7532ca3f509adc7239c
trevismcgill <trevismcgill25@gmail.com>
2020-10-28 14:35:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.className = "victory";
+newHeader.id = "victory";
+newHeader.innerHTML = "Trevis is the champion";
+
+
